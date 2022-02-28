9329f5e8c5a96d3bec44c87b09ec4cd25ad8f555
Udit Choudhary <uditchoudhary@gmail.com>
2022-02-28 13:18:28 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import logo from "./logo.jpeg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={require("./yuuva.PNG")} style={{"width": "100px"}} alt="logo"/>
+        <img
+          src={require("./yuuva.PNG")}
+          style={{ width: "100px" }}
+          alt="logo"
+        />
         <h1>Coming Soon</h1>
         <p>We are currently working on creating our new website.</p>
         <p>We will be launching soon</p>
-        <i class="fa-brands fa-instagram"></i>
-        <i class="bi bi-instagram"></i>
-        <i class="bi bi-facebook"></i>
-        <i class="bi bi-whatsapp"></i>
+        <i className="fa-brands fa-instagram"></i>
+        <i className="bi bi-instagram"></i>
+        <i className="bi bi-facebook"></i>
+        <i className="bi bi-whatsapp"></i>
       </header>
     </div>
   );
