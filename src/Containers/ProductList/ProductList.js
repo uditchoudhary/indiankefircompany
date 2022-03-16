19c475855b6837fbb3ecb4f7a8d4254f93a3265f
Udit Choudhary <uditchoudhary@gmail.com>
2022-03-16 19:11:42 +1300

    commented unwanted code

--- src/Containers/ProductList/ProductList.js
+++ src/Containers/ProductList/ProductList.js
@@ -1,17 +1,17 @@
 import ProductCard from "../../Components/ProductCard/ProductCard"
 
-const list = [
-    {
-        id:1,
-        name: "random",
-        image: "",
-        details: "",
-        price: "",
-        availableSize: [1,2,3],
-        reviews: "",
-        
-    }
-]
+// const list = [
+//     {
+//         id:1,
+//         name: "random",
+//         image: "",
+//         details: "",
+//         price: "",
+//         availableSize: [1,2,3],
+//         reviews: "",
+
+//     }
+// ]
 
 const ProductList = () => {
     return (
